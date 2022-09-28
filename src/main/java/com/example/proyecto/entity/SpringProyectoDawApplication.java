0854f3570898f0d3c5ade64829dbc538014ab8b8
CECHQ007 <carlos-eduar201@hotmail.com>
2022-09-27 20:37:40 -0500

    cambios

---
+++ src/main/java/com/example/proyecto/entity/SpringProyectoDawApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyectoDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyectoDawApplication.class, args);
+	}
+
+}
